be3bef513c1e91285ac9c03d0c0ceb0aafd88848
Logan Brewer <derpypineapples645@gmail.com>
2023-10-29 17:44:11 -0400

    Add project files.

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NoteTaker.Models;
+
+namespace GuitarShop.Controllers
+{
+    public class NotesController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Route("[controller]s/{id?}")]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
